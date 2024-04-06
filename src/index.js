aeb730c44ac5c19edddc51763c703d816a15f799
RedEyesNCode <ashutosh.singh19071999@gmail.com>
2024-04-06 11:51:59 +0530

    Add react router

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import ProductDetail from "./components/product/ProductDetail";
+import ProductByCategory from "./components/product/ProductByCategory";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes>
+      <Route exact path="/" element={<App />} />
+
+      <Route exact path="/product-detail" element={<ProductDetail />} />
+      <Route path="/product-category" element={<ProductByCategory />} />
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
+
+reportWebVitals();
