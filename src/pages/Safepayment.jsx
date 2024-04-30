5b87aaa47289718d082e699fe56dac52ec22ae93
Rohit Chouhan <Rohitchouhan201090@gmail.com>
2024-04-30 17:08:38 +0530

    Home page completed

--- src/pages/Safepayment.jsx
+++ src/pages/Safepayment.jsx
@@ -81,23 +81,23 @@ useEffect(() => {
 }, []);
   return (
     <div className="container-fluid" style={{overflow:"hidden"}}>
-      <motion.div variants={mainvariants} initial="initial"  animate={scrollPosition > 1750? "animate" : "initial"}  className="row box4" style={{display:"flex", flexDirection:"row",overflow:"hidden"}}>
+      <motion.div variants={mainvariants} initial="initial"  animate={scrollPosition > 1650? "animate" : "initial"}  className="row box4" style={{display:"flex", flexDirection:"row",overflow:"hidden"}}>
        
-        <motion.div variants={leftVariants} initial="initial"  animate={scrollPosition > 1750 ? "animate" : "initial"}  className="col-md text-center box4a" data-aos="slide-right" style={{ display: 'flex', flexDirection:"column", justifyContent: 'center', alignItems: 'center' }}>
+        <motion.div variants={leftVariants} initial="initial"  animate={scrollPosition > 1650 ? "animate" : "initial"}  className="col-md text-center box4a" data-aos="slide-right" style={{ display: 'flex', flexDirection:"column", justifyContent: 'center', alignItems: 'center' }}>
             <GiCityCar  style={{ color: 'rgb(2, 34, 31)', fontSize: '60px', justifyItems:"center"}}/>
           <h5 style={{ fontWeight: 'bold', fontSize: '24px' }}>Free Shipping</h5>
           <p style={{paddingLeft:"20px",paddingRight:"20px",paddingBottom:"20px"}}>Adipiscing sociosqu placerat <br /> platea rutrum ut eros vestibulum posuere dictum
             blandit massa porttitor amet.</p>
         </motion.div>
 
-        <motion.div variants={betweenVariants} initial="initial"  animate={scrollPosition > 1750 ? "animate" : "initial"}  className="col-md text-center box4b" data-aos="slide-up" style={{ display: 'flex', flexDirection:"column", justifyContent: 'center', alignItems: 'center' }}>
+        <motion.div variants={betweenVariants} initial="initial"  animate={scrollPosition > 1650 ? "animate" : "initial"}  className="col-md text-center box4b" data-aos="slide-up" style={{ display: 'flex', flexDirection:"column", justifyContent: 'center', alignItems: 'center' }}>
           <FaFileInvoiceDollar style={{ fontSize: '40px' }} />
           <h5 style={{ fontWeight: 'bold', fontSize: '24px' }}>Safe Payment</h5>
           <p style={{paddingLeft:"20px",paddingRight:"20px",paddingBottom:"20px"}}>Adipiscing sociosqu placerat <br /> platea rutrum ut eros vestibulum posuere dictum
             blandit massa porttitor amet.</p>
         </motion.div>
 
-        <motion.div variants={rightVariants} initial="initial"  animate={scrollPosition > 1750 ? "animate" : "initial"} className="col-md text-center box4c" data-aos="slide-left" style={{ display: 'flex', flexDirection:"column", justifyContent: 'center', alignItems: 'center' }}>
+        <motion.div variants={rightVariants} initial="initial"  animate={scrollPosition > 1650 ? "animate" : "initial"} className="col-md text-center box4c" data-aos="slide-left" style={{ display: 'flex', flexDirection:"column", justifyContent: 'center', alignItems: 'center' }}>
           <i className="fa-solid fa-phone-volume" style={{ color: 'rgb(2, 34, 31)', fontSize: '50px' }}></i>
           <MdOutlineWifiCalling3  style={{ color: 'rgb(2, 34, 31)', fontSize: '40px' }}/>
           <h5 style={{ fontWeight: 'bold', fontSize: '24px' }}>Online Support</h5>
